d0ecb5342bf1f12621e6d0412d346cb951a5f0d3
alicranck <alicranck@gmail.com>
2026-01-07 12:12:23 +0200

    added openvino siglip

--- vision_tools/core/tools/embedder.py
+++ vision_tools/core/tools/embedder.py
@@ -1,7 +1,11 @@
 from typing import Any, List
 import clip
 import torch
+import logging
+from pathlib import Path
 from transformers import AutoModel, AutoProcessor, AutoTokenizer
+from optimum.intel.openvino import OVModelForZeroShotImageClassification
+from optimum.intel.openvino.configuration import OVWeightQuantizationConfig
 from huggingface_hub import snapshot_download
 from PIL import Image
 import numpy as np
@@ -71,100 +75,37 @@ class CLIPEmbedder(BaseVisionTool):
         return []
 
 
-class JinaEmbedder(BaseVisionTool):
-    """
-    Tool for generating multimodal embeddings using jinaai/jina-embeddings-v4.
-    """
-    def __init__(self, model_id: str = "jinaai/jina-embeddings-v4", 
-                                config: dict = None, device: str = 'cpu'):
-        if config is None:
-            config = {}
-        super().__init__(model_id, config, device)
-
-    def _load_model(self) -> Any:
-        model_path = self._resolve_model_path(self.model_id)
-        model = AutoModel.from_pretrained(
-            model_path, 
-            trust_remote_code=True, 
-            device_map=self.device
-        )
-        return model
-
-    def download_ckpt(self, model_id: str, destination: list) -> str:
-        local_dir = snapshot_download(repo_id=model_id, local_dir=destination)
-        return local_dir
-
-    def preprocess(self, frame: np.ndarray) -> Any:
-        pil_image = Image.fromarray(frame)
-        return pil_image
-
-    def preprocess_text(self, text: str) -> Any:
-        return text
-
-    def inference(self, model_inputs: Any) -> Any:
-        embeddings = self.model.encode_image(model_inputs, task="retrieval")
-        return embeddings
-
-    def postprocess(self, raw_output: Any, original_shape: tuple) -> dict:
-        if isinstance(raw_output, torch.Tensor):
-             raw_output = raw_output.cpu().numpy()
-        
-        if isinstance(raw_output, np.ndarray):
-             embedding = raw_output.tolist()
-             if isinstance(embedding[0], list): # Batch size 1
-                 embedding = embedding[0]
-        else:
-            embedding = raw_output
-
-        return {"embedding": embedding}
-
-    def encode_text(self, text: str) -> List[float]:
-        if not self.loaded:
-            raise RuntimeError(f"ERROR: {self.tool_name} is not loaded. Call .load_tool() first.")
-        
-        embeddings = self.model.encode([text], task="retrieval", prompt_name="query")
-        return embeddings[0].tolist()
-
-    @property
-    def output_keys(self) -> List[ToolKey]:
-        return [
-            ToolKey("embedding", list, "Jina embedding vector of the image or text")
-        ]
-
-    @property
-    def processing_input_keys(self) -> List[ToolKey]:
-        return []
-
-    @property
-    def config_keys(self) -> List[ToolKey]:
-        return []
-
-
 class SigLIP2Embedder(BaseVisionTool):
     """
     Tool for generating multimodal embeddings using google siglip2.
     """
     def __init__(self, model_id: str = "google/siglip2-base-patch16-384",
-                                     config: dict = None, device: str = 'cpu'):
+                        config: dict = None, device: str = 'cpu'):
         if config is None:
             config = {}
+        self.dummy_text = ["dummy"]
         super().__init__(model_id, config, device)
 
     def _load_model(self) -> Any:
+        
         model_path = self._resolve_model_path(self.model_id)
+
         model = AutoModel.from_pretrained(model_path).eval()
         model.to(self.device)
+
         self.processor = AutoProcessor.from_pretrained(model_path, use_fast=True)
         self.tokenizer = AutoTokenizer.from_pretrained(model_path)
+
         return model
 
-    def download_ckpt(self, model_id: str, destination: list) -> str:
+    def download_ckpt(self, model_id: str, destination: str) -> str:
         local_dir = snapshot_download(repo_id=model_id, local_dir=destination)
         return local_dir
 
     def preprocess(self, frame: np.ndarray) -> Any:
         pil_image = Image.fromarray(frame)
-        inputs = self.processor(images=[pil_image], return_tensors="pt")
+        inputs = self.processor(images=[pil_image], text=self.dummy_text,
+                                                         return_tensors="pt")
         return inputs.to(self.device)
 
     def preprocess_text(self, text: str) -> Any:
@@ -204,3 +145,66 @@ class SigLIP2Embedder(BaseVisionTool):
     @property
     def config_keys(self) -> List[ToolKey]:
         return []
+
+
+class OVSigLIP2Embedder(SigLIP2Embedder):
+    """
+    OpenVINO optimized version of SigLIP2Embedder.
+    """
+
+    def __init__(self, model_id: str = "google/siglip2-base-patch16-384",
+                        config: dict = None, device: str = 'cpu'):
+        super().__init__(model_id, config, device)
+        self.dummy_image = Image.new("RGB", (384, 384))
+
+    def _load_model(self) -> Any:
+        
+        model_path = self._resolve_model_path(self.model_id)
+        
+        model = self.export_to_openvino(model_path)
+        
+        self.processor = AutoProcessor.from_pretrained(model_path, use_fast=True)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_path)
+        
+        return model
+
+    def export_to_openvino(self, model_path: str):
+        
+        ov_model_path = Path(model_path) / "ov"
+
+        quangization_config = OVWeightQuantizationConfig()
+
+        if ov_model_path.exists():
+             model = OVModelForZeroShotImageClassification.from_pretrained(ov_model_path, 
+                                                                            quantization_config=quangization_config,
+                                                                            device=self.device)
+        else:
+             model = OVModelForZeroShotImageClassification.from_pretrained(model_path, export=True, quantization_config=quangization_config,
+                                                                            device=self.device)
+             model.save_pretrained(ov_model_path)
+        
+        return model
+
+    def inference(self, model_inputs: Any) -> Any:
+        results = self.model(**model_inputs)
+        return results.image_embeds
+
+    def preprocess_text(self, text: str) -> Any:
+        inputs = self.processor(images=[self.dummy_image], text=[text],
+                                 max_length=64, padding="max_length", 
+                                 return_tensors="pt")
+        return inputs.to(self.device)
+
+    def encode_text(self, text: str) -> List[float]:
+        if not self.loaded:
+            raise RuntimeError(f"ERROR: {self.tool_name} is not loaded. Call .load_tool() first.")
+        
+        text_input = self.preprocess_text(text)
+        results = self.model(**text_input)
+        embedding = results.text_embeds
+        
+        if isinstance(embedding, torch.Tensor):
+            embedding = embedding.cpu().numpy()
+            
+        return embedding.squeeze().tolist()
+
